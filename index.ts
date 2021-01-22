7fe5febd63df6441de18c9daa51109940f8c0fcf
ninest <parth.kabra@gmail.com>
2021-01-22 10:42:54 +0800

    Add UI

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log('hello')
+const $countryCodeField: HTMLInputElement = document.querySelector("#code");
+const $phoneNumberField: HTMLInputElement = document.querySelector("#phone");
+const $chatButton = document.querySelector("#chat");
+
+$chatButton.addEventListener("click", () => {
+  alert($countryCodeField.value);
+});
