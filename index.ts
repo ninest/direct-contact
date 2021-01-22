a84699d9fc8ea35a4794f9433ef287e455750680
ninest <parth.kabra@gmail.com>
2021-01-22 12:51:36 +0800

    Add WhatsApp link and persist country code

--- index.ts
+++ index.ts
@@ -1,7 +1,32 @@
 const $countryCodeField: HTMLInputElement = document.querySelector("#code");
 const $phoneNumberField: HTMLInputElement = document.querySelector("#phone");
-const $chatButton = document.querySelector("#chat");
+const $form = document.querySelector("#form");
 
-$chatButton.addEventListener("click", () => {
-  alert($countryCodeField.value);
+$form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  
+  const countryCode = $countryCodeField.value;
+  const phoneNumber = $phoneNumberField.value;
+
+  // Both are required
+  if (!countryCode || !phoneNumber) return;
+
+  // Save country code
+  localStorage.setItem("country-code", countryCode);
+
+  const fullContactNumber = `${countryCode}${phoneNumber}`.replace(
+    /[^0-9]/g,
+    ""
+  );
+
+  console.log(fullContactNumber);
+  const url = `https://web.whatsapp.com/send?phone=${countryCode}${phoneNumber}`;
+  window.open(url, "_blank");
+});
+
+window.addEventListener("load", () => {
+  console.log("aer");
+
+  // Set saved country code
+  $countryCodeField.value = localStorage.getItem("country-code");
 });
